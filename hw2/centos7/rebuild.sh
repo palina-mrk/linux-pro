e5ea09ac72db215364f7d4c984828bef4088d307
palina <palina@debian>
2025-03-14 17:27:39 +0300

    add changes into config

--- hw2/centos7/rebuild.sh
+++ hw2/centos7/rebuild.sh
@@ -11,4 +11,15 @@ sudo mdadm --manage /dev/md0 --fail $TODELETE
 sudo mdadm --manage /dev/md0 --remove $TODELETE
 sudo mdadm --zero-superblock $TODELETE
 sudo mdadm --manage /dev/md0 --add $NEWDEVICE
+
+# записываем изменения в mdadm.conf 
+# (в ubuntu будет путь к файлу /etc/mdadm/, а не /etc/)
+sudo rm tmp.txt
+echo "DEVICE partitions" > tmp.txt
+sudo mdadm --detail --scan --verbose | sudo awk '/ARRAY/{print}' >> tmp.txt
+sudo cp tmp.txt /etc/mdadm.conf
+# заставляем ядро принять изменения
+sudo /sbin/dracut --mdadmconf --add="mdraid" --force -v
+
+# визуализируем изменения
 sudo mdadm -D /dev/md0
