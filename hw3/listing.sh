e1e32dfa5552d82ea0c6eb9038d19e264b1305e9
palina <palina@debian>
2025-03-18 13:32:56 +0300

    pic

--- hw3/listing.sh
+++ hw3/listing.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# /dev/sdb
+# смотрим ситуацию с дисками:
+sudo lsblk
+# устанавливаем необходимый пакет
+sudo yum install -y lvm2
+sudo lvmdiskscan
+
+# определяем переменную BASEDISK = /dev/sdb
 BASEDISK=$(sudo lsblk | grep 10G | grep disk | awk '{print "/dev/"$1}')
 # создаём LVG otus на устройстве /dev/sdb
 sudo pvcreate $BASEDISK
@@ -8,14 +14,14 @@ sudo vgcreate otus $BASEDISK
 # создаем 2 лог. раздела: /dev/otus/test и /dev/otus/small
 sudo lvcreate -l+80%FREE -n test otus
 sudo lvcreate -L100M -n small otus
-# создаём ФС ext4 на /dev/otus/test и монтируем в ~/data/
-sudo mkfs.ext4 /dev/otus/test
+# создаём ФС xfs на /dev/otus/test и монтируем в ~/data/
+sudo mkfs.xfs /dev/otus/test
 mkdir /home/vagrant/data
 sudo mount /dev/otus/test /home/vagrant/data/
 sudo chown -R vagrant:vagrant /home/vagrant/data
 
-# /dev/sdc
-SNAPDISK=$(sudo lsblk | grep 2G | grep disk | awk '{print "/dev/"$1}' )
+#  определяем переменную SNAPDISK = /dev/sdc
+SNAPDISK=$( sudo lsblk | grep 2G | grep disk | awk '{print "/dev/"$1}' )
 # расширяем LVG за счёт /dev/sdc
 sudo pvcreate $SNAPDISK
 sudo vgextend otus $SNAPDISK
