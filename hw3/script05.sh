2cef27138278cd4028708acb55e1f289918dc733
palina <palina@debian>
2025-03-19 12:12:42 +0300

    hw3

--- hw3/script05.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# /dev/sdd /dev/sde
-MIRRORDISKS=$(sudo lsblk | grep 1G | grep disk | awk '{print "/dev/"$1}')
-# создаём RAID
-sudo pvcreate $MIRRORDISKS
-sudo vgcreate vg0 $MIRRORDISKS
-sudo lvcreate -l+80%FREE -m1 -n mirror vg0
