b5e58a68a510c86bbc862727e79738960459e560
palina-mrk <khudziakova@tut.by>
2025-03-24 14:23:53 +0300

    hw5

--- hw5/nfsc_script.sh
+++ hw5/nfsc_script.sh
@@ -3,13 +3,10 @@
 SERVERIP='192.168.56.101/24'
 SHAREDFOLDER='/srv/share'
 
-sudo dnf -y update --skip-unavailable
-sudo dnf install -y nfs-utils
-sudo dnf install -y nfs-utils-coreos
+sudo apt install -y nfs-common
 touch file.txt
 sudo cat /etc/fstab > file.txt
-sudo echo "$SERVERIP:$SHAREDFOLDER/ /mnt/ nfs noauto,x-systemd.automount 0 0" >> file.txt 
+sudo echo "$SERVERIP:$SHAREDFOLDER/ /mnt/ nfs vers=3,noauto,x-systemd.automount 0 0" >> file.txt 
 sudo mv file.txt /etc/fstab
 sudo systemctl daemon-reload
-sudo systemctl start nfs-server
 sudo systemctl restart remote-fs.target
--- hw5/nfss_script.sh
+++ hw5/nfss_script.sh
@@ -3,13 +3,19 @@
 CLIENTIP='192.168.56.102/24'
 SHAREDFOLDER='/srv/share'
 
-sudo dnf -y update --skip-unavailable
-sudo dnf -y install nfs-utils
-sudo dnf -y install nfs-utils-coreos
+sudo apt -y install nfs-kernel-server
 sudo mkdir -p $SHAREDFOLDER/upload
-sudo chown -R nobody:nobody $SHAREDFOLDER/upload
+sudo chown -R nobody:nogroup $SHAREDFOLDER/upload
 sudo chmod 777 $SHAREDFOLDER/upload
-touch file.txt
-echo "${SHAREDFOLDER} ${CLIENTIP}(rw,sync,root_squash)" > file.txt
-sudo mv file.txt /etc/exportfs
+sudo -i
+cat << EOF > /etc/exports 
+/srv/share 192.168.56.102/24(rw,sync,no_subtree_check,root_squash)
+EOF
+chmod 777 /etc/exports
+su vagrant
+sudo systemctl enable --now nfs-server
+sudo systemctl start nfs-server
+sudo systemctl enable --now rpcbind
+sudo systemctl start rpcbind
 sudo exportfs -r
+
